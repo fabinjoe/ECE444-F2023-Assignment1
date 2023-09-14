6fd1fddf55994bfce235f9bde8ee867e982f53b0
Fabin Joe <66658906+fabinjoe@users.noreply.github.com>
2023-09-14 19:09:26 -0400

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,11 +1,16 @@
 class Utils:
   def reversed(number: int) -> int:
+    if type(number) != int:
+      raise TypeError("Wanted an integer.")
+      
     str_number = str(number)
     return int(str_number[::-1])
     
   def formatter(number: int, base: int) -> int:
+    if type(number) != int:
+      raise TypeError("Wanted an integer.")
     if base not in (2, 8):
-      return "Not compatible base formattor."
+      raise TypeError("base = 2 or 8")
     if base == 2:
       return bin(number)
     elif base == 8:
